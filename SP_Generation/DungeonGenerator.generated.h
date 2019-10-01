223507dce55021a96355bc01a0a4e2c00ad0348e
BraedenMiles <braedenstillabower@cmail.carleton.ca>
2019-10-01 09:45:14 -0400

    Bitch ass dungeon generator

---
+++ SP_Generation/DungeonGenerator.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SP_GENERATION_DungeonGenerator_generated_h
+#error "DungeonGenerator.generated.h already included, missing '#pragma once' in DungeonGenerator.h"
+#endif
+#define SP_GENERATION_DungeonGenerator_generated_h
+
+#define SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_RPC_WRAPPERS
+#define SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesADungeonGenerator(); \
+	friend struct Z_Construct_UClass_ADungeonGenerator_Statics; \
+public: \
+	DECLARE_CLASS(ADungeonGenerator, AActor, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/SP_Generation"), NO_API) \
+	DECLARE_SERIALIZER(ADungeonGenerator)
+
+
+#define SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesADungeonGenerator(); \
+	friend struct Z_Construct_UClass_ADungeonGenerator_Statics; \
+public: \
+	DECLARE_CLASS(ADungeonGenerator, AActor, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/SP_Generation"), NO_API) \
+	DECLARE_SERIALIZER(ADungeonGenerator)
+
+
+#define SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ADungeonGenerator(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ADungeonGenerator) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ADungeonGenerator); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ADungeonGenerator); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ADungeonGenerator(ADungeonGenerator&&); \
+	NO_API ADungeonGenerator(const ADungeonGenerator&); \
+public:
+
+
+#define SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ADungeonGenerator(ADungeonGenerator&&); \
+	NO_API ADungeonGenerator(const ADungeonGenerator&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ADungeonGenerator); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ADungeonGenerator); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ADungeonGenerator)
+
+
+#define SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_PRIVATE_PROPERTY_OFFSET
+#define SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_9_PROLOG
+#define SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_PRIVATE_PROPERTY_OFFSET \
+	SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_RPC_WRAPPERS \
+	SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_INCLASS \
+	SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_PRIVATE_PROPERTY_OFFSET \
+	SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_INCLASS_NO_PURE_DECLS \
+	SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SP_GENERATION_API UClass* StaticClass<class ADungeonGenerator>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID SP_Attempt_1_1_4_23___2_Source_SP_Generation_DungeonGenerator_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
